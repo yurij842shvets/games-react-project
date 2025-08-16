12c95ca0e84f7236816d44d13aa4e1a0bf2ae596
yurij842shvets <yurij2011shvets@gmail.com>
2025-08-16 22:54:25 +0300

    added text in  titiles jsx needs fixation

--- src/about-us-page-section/main/Titles.jsx
+++ src/about-us-page-section/main/Titles.jsx
@@ -1,28 +1,73 @@
+import styled from "styled-components";
 
+const Title = styled.h2`
+  font-weight: bold;
+  color: #fff;
+  width: 500px;
+  font-size: 30px;
+  font-family: "Poppins", sans-serif;
+  @media screen and (max-width: 387px) {
+    width: 200px;
+  }
+`;
+
+const Description = styled.p`
+  color: #fff;
+  width: 400px;
+  font-size: 14px;
+  font-family: "Poppins", sans-serif;
+  @media screen and (max-width: 557px) {
+    width: 250px;
+  }
+`;
+
+const Flex = styled.p`
+  display: flex;
+`;
+
+const ArrowContainer = styled.div`
+  position: relative;
+  margin: 0 25px;
+`;
+const Arrow = styled.div`
+  &::after {
+    width: 10px;
+    height: 2px;
+    background-color: #fff;
+    transform: rotate(-45deg);
+  }
+`;
 
 export default function Titles() {
-
-    return(
-
-        <>
+  return (
+    <>
+      <div>
         <div>
-            <div>
-               <div>
-                <p>
-                    <span>Home</span>
-                    <div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                    <span>About us</span>
-                </p>
-               </div>
-            </div>
+          <Flex>
+            <span>Home</span>
+            <ArrowContainer>
+              <Arrow></Arrow>
+            </ArrowContainer>
+            <span>About us</span>
+          </Flex>
 
-            <div>
+          <Title>
+            We are more than just a gaming platform — we are a community built
+            by players, for players. We bring games to life in a way that
+            inspires and unites."
+          </Title>
 
-            </div>
+          <Description>
+            Our mission is to connect people through immersive experiences,
+            where creativity, competition, and fun come together. Every project
+            we work on is fueled by passion and a deep love for the gaming
+            world. With us, you don’t just play — you become part of something
+            bigger.
+          </Description>
         </div>
-        </>
-    )
+
+        <div></div>
+      </div>
+    </>
+  );
 }
